add5369a4bb306a96a0d034cb41db43886426076
Will Harrison <willharrison2016@gmail.com>
2025-03-15 23:16:40 -0500

    Create Icons.js

---
+++ components/Icons.js
@@ -0,0 +1,16 @@
+// components/Icons.js
+
+import React from "react";
+
+// Simple placeholders for demonstration
+export const UploadIcon = () => <span style={{ marginRight: 6 }}>📤</span>;
+export const SearchIcon = () => <span style={{ marginRight: 6 }}>🔍</span>;
+export const HoopIcon = () => <span style={{ marginRight: 6 }}>🎯</span>;
+export const LogoutIcon = () => <span style={{ marginRight: 6 }}>🔓</span>;
+export const LoginIcon = () => <span style={{ marginRight: 6 }}>🔒</span>;
+export const PlusIcon = () => <span style={{ marginRight: 6 }}>➕</span>;
+export const MenuIcon = () => <span style={{ marginRight: 6 }}>☰</span>;
+export const MoonIcon = () => <span style={{ marginRight: 6 }}>🌙</span>;
+export const SunIcon = () => <span style={{ marginRight: 6 }}>☀️</span>;
+export const SettingsIcon = () => <span style={{ marginRight: 6 }}>⚙️</span>;
+export const ProfileIcon = () => <span style={{ marginRight: 6 }}>👤</span>;
