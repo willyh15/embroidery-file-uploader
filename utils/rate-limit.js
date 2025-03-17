f65d1f8dcaedf9bbed01ce830fce427c8a8503a4
Will Harrison <willharrison2016@gmail.com>
2025-03-16 23:11:57 -0500

    Update rate-limit.js

--- utils/rate-limit.js
+++ utils/rate-limit.js
@@ -2,11 +2,11 @@
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
-  url: process.env.KV_REST_API_URL,  // make sure these env variables are set in your production environment
+  url: process.env.KV_REST_API_URL,
   token: process.env.KV_REST_API_TOKEN,
 });
 
-export async function rateLimit(ip, limit = 10, timeframe = 60) {
+export const rateLimit = async (ip, limit = 10, timeframe = 60) => {
   const key = `rate-limit:${ip}`;
   const currentCount = await redis.get(key);
   const count = currentCount ? parseInt(currentCount, 10) : 0;
@@ -19,4 +19,4 @@ export async function rateLimit(ip, limit = 10, timeframe = 60) {
   await redis.expire(key, timeframe);
 
   return true;
-}
+};
