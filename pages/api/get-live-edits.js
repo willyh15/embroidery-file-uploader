ac01f7c7a7f85b20413217550bcfc85f010f1a05
Will Harrison <willharrison2016@gmail.com>
2025-03-20 21:04:58 -0500

    Update get-live-edits.js

--- pages/api/get-live-edits.js
+++ pages/api/get-live-edits.js
@@ -1,9 +1,9 @@
 import { Redis } from "@upstash/redis";
 
-// Initialize Upstash Redis client using environment variables
+// Initialize Upstash Redis client using new environment variables
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
 });
 
 export default async function handler(req, res) {
