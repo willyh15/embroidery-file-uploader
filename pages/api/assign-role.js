e6e962139b89789cb356d2ee4d8feadb7d0c9718
Will Harrison <willharrison2016@gmail.com>
2025-03-24 23:05:25 -0500

    Update assign-role.js

--- pages/api/assign-role.js
+++ pages/api/assign-role.js
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]";
+import { authOptions } from "./auth/[...nextauth]";
 
 const redis = new Redis({
   url: process.env.KV_REST_API_URL,
