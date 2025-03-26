f968691df3a265d073f3833e2b7b665feadacbe2
Will Harrison <willharrison2016@gmail.com>
2025-03-25 21:12:09 -0500

    Update FilePreviewCard.js

--- components/FilePreviewCard.js
+++ components/FilePreviewCard.js
@@ -1,3 +1,4 @@
+{file.status === "Converted" && <span className="badge success">DST/PES Ready</span>}
 {file.convertedUrl && (
   <a href={file.convertedUrl} target="_blank" rel="noopener noreferrer">
     <Button>Download</Button>
