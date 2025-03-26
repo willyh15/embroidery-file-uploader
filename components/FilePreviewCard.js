7760dfbef7d93ebc16079cd9e9b4c492c3f59128
Will Harrison <willharrison2016@gmail.com>
2025-03-25 21:10:48 -0500

    Update FilePreviewCard.js

--- components/FilePreviewCard.js
+++ components/FilePreviewCard.js
@@ -1,2 +1,6 @@
-// Component: FilePreviewCard
+{file.convertedUrl && (
+  <a href={file.convertedUrl} target="_blank" rel="noopener noreferrer">
+    <Button>Download</Button>
+  </a>
+)}
 export default function FilePreviewCard() {}
