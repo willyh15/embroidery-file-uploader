6874cd9f48d133ef0e4676721d077914f3306583
Will Harrison <willharrison2016@gmail.com>
2025-03-27 12:34:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -315,3 +315,6 @@ const updateFileStatus = (fileUrl, status, convertedUrl = null, stage = null) =>
 }
 
 export default dynamic(() => Promise.resolve(Home), { ssr: false });
+}
+
+export default dynamic(() => Promise.resolve(Home), { ssr: false });
