ddd1a62b452d77dc1edaf929cd5ae686f866c21a
Will Harrison <willharrison2016@gmail.com>
2025-03-31 00:11:02 -0500

    Update ConvertAllButton.js

--- components/ConvertAllButton.js
+++ components/ConvertAllButton.js
@@ -1,2 +1,12 @@
 // Component: ConvertAllButton
-export default function ConvertAllButton() {}
+import Button from "./Button";
+
+export default function ConvertAllButton({ onConvertAll }) {
+  if (!onConvertAll) return null;
+
+  return (
+    <div style={{ margin: "1rem 0" }}>
+      <Button onClick={onConvertAll}>Convert All Files</Button>
+    </div>
+  );
+}
