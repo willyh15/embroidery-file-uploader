1e5c41379ba9b87e6f6d7f1287e00e24f5f98fe3
Will Harrison <willharrison2016@gmail.com>
2025-05-04 01:12:30 -0500

    Delete pages/api directory

--- pages/api/auth/[...nextauth].js
+++
@@ -1,7 +0,0 @@
-// pages/api/auth/[...nextauth].js (DISABLED)
-
-// Auth temporarily disabled - this file can be deleted or left as-is without being used.
-
-export default function handler(req, res) {
-  return res.status(200).json({ message: "Auth temporarily disabled" });
-}
