75368304a0fb95f8b69c4854c776b9a0c5eaa94b
Will Harrison <willharrison2016@gmail.com>
2025-05-04 01:14:37 -0500

    Delete components/Loader.js

--- components/Loader.js
+++
@@ -1,5 +0,0 @@
-export default function Loader({ size = "40px", color = "var(--accent)" }) {
-  return (
-    <div className="loader" style={{ width: size, height: size, borderColor: color }}></div>
-  );
-}
