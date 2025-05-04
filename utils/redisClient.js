6450b7178fccc949d0992be3cc9e8bb883ae00bc
Will Harrison <willharrison2016@gmail.com>
2025-05-04 01:12:52 -0500

    Delete utils directory

--- utils/redisClient.js
+++
@@ -1,7 +0,0 @@
-// utils/redisClient.js
-import { Redis } from "@upstash/redis";
-
-export const redis = new Redis({
-  url: process.env.KV_REST_API_URL,    // Ensure these env variables are set in your deployment
-  token: process.env.KV_REST_API_TOKEN,
-});
