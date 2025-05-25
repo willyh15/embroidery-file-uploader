71d09ed3ceaee517748369fb481cb6682a05842e
Will Harrison <willharrison2016@gmail.com>
2025-05-24 23:56:38 -0500

    Update PaginationControls.js

--- components/PaginationControls.js
+++ components/PaginationControls.js
@@ -20,25 +20,21 @@ export default function PaginationControls({
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
         className={`
-          px-4 py-2 rounded-lg text-sm
-          ${currentPage === 1
-            ? "bg-gray-300 text-gray-500 cursor-not-allowed"
-            : "bg-blue-500 text-white hover:bg-blue-600"}
+          btn btn-secondary
+          ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""}
         `}
       >
         Previous
       </button>
-      <span className="text-white">
+      <span className="text-[var(--primary-text)]">
         Page {currentPage} of {totalPages}
       </span>
       <button
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
         className={`
-          px-4 py-2 rounded-lg text-sm
-          ${currentPage === totalPages
-            ? "bg-gray-300 text-gray-500 cursor-not-allowed"
-            : "bg-blue-500 text-white hover:bg-blue-600"}
+          btn btn-secondary
+          ${currentPage === totalPages ? "opacity-50 cursor-not-allowed" : ""}
         `}
       >
         Next
