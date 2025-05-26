da3c339a5e201ae00158faf11f574cac649076e2
Will Harrison <willharrison2016@gmail.com>
2025-05-25 20:40:44 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// tailwind.config.js
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
@@ -9,16 +8,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primaryBg:   "#33202E",
-        primaryTxt:  "#FBD3E0",
-        secondaryBg: "#44303D",
-        accent:      "#F4A9A8",
-        accentAlt:   "#C1D7C3",
-        border:      "rgba(255,255,255,0.2)",
-        shadow:      "rgba(0,0,0,0.5)",
-        neonPink:    "#FF488E",
-        neonCyan:    "#48FFE6",
-        neonYellow:  "#FFC948",
+        'primary-bg':   "#33202E",
+        'primary-txt':  "#FBD3E0",
+        'secondary-bg': "#44303D",
+        'accent':       "#F4A9A8",
+        'accent-alt':   "#C1D7C3",
+        'border':       "rgba(255,255,255,0.2)",
+        'shadow':       "rgba(0,0,0,0.5)",
+        'neon-pink':    "#FF488E",
+        'neon-cyan':    "#48FFE6",
+        'neon-yellow':  "#FFC948",
       },
       fontFamily: {
         sans:    ["Montserrat", ...defaultTheme.fontFamily.sans],
