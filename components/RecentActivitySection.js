c7393d939d0808835c551a9028835ba6c6ba51c1
Will Harrison <willharrison2016@gmail.com>
2025-05-27 10:15:35 -0500

    Create RecentActivitySection.js

---
+++ components/RecentActivitySection.js
@@ -0,0 +1,5 @@
+import RecentActivityPanel from "./RecentActivityPanel";
+
+export default function RecentActivitySection({ uploadedFiles }) {
+  return <RecentActivityPanel uploadedFiles={uploadedFiles} />;
+}
