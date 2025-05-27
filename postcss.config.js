ed43b121c9ea8e49e71fca44e271f564578eada1
Will Harrison <willharrison2016@gmail.com>
2025-05-26 20:16:34 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
 module.exports = {
-  plugins: [
-    require("@tailwindcss/postcss"),
-    require("autoprefixer"),
-  ],
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
 };
